e4fd1dbf7722f41e2947bb78332a9d7798672219
jecolema <jecolema@redhat.com>
2016-06-16 08:22:16 -0400

    added GetSyncRejectCount

--- pyeloqua/pyeloqua.py
+++ pyeloqua/pyeloqua.py
@@ -531,6 +531,25 @@ class Eloqua(object):
 
         return totalResults
 
+    def GetSyncRejectCount(self, syncObject={}, syncURI=''):
+
+        if ('uri' not in syncObject):
+            if (len(syncURI)==0):
+                raise Exception("Must include a valid syncObject or syncURI")
+            else:
+                uri = syncURI
+        else:
+            uri = syncObject['uri']
+
+        url = self.bulkBase + uri + '/rejects?limit=1'
+
+        req = requests.get(url, auth=self.auth)
+
+        totalRejects = req.json()['totalResults']
+
+        return totalRejects
+
+
     def GetSyncedData(self, defObject={}, defURI='', limit=50000, initOffset=0):
 
         """
