a7e2446f884adf5c15552b2728873d216923c872
colemanja91 <colemanja91@gmail.com>
2016-11-21 20:47:56 -0500

    check exception with no cdoId

--- pyeloqua/test/test_Eloqua_GetFields.py
+++ pyeloqua/test/test_Eloqua_GetFields.py
@@ -12,3 +12,11 @@ def test_GetFields_EntityException(mock_get):
     mock_get.return_value.json.return_value = elqLogin
     elq = Eloqua(company = 'test', username = 'test', password = 'test')
     x = elq.GetFields(entity='bad')
+
+@patch('pyeloqua.pyeloqua.requests.get')
+@raises(Exception)
+def test_GetFields_CdoIdException(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    x = elq.GetFields(entity='customObjects')
