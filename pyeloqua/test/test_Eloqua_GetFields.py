79d0ab6e14f05a8fedd233b5026a7f6e7bcbecc8
colemanja91 <colemanja91@gmail.com>
2016-11-21 20:50:50 -0500

    handling bad status code

--- pyeloqua/test/test_Eloqua_GetFields.py
+++ pyeloqua/test/test_Eloqua_GetFields.py
@@ -20,3 +20,12 @@ def test_GetFields_CdoIdException(mock_get):
     mock_get.return_value.json.return_value = elqLogin
     elq = Eloqua(company = 'test', username = 'test', password = 'test')
     x = elq.GetFields(entity='customObjects')
+
+@patch('pyeloqua.pyeloqua.requests.get')
+@raises(Exception)
+def test_GetFields_BadStatusException(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    mock_get.return_value = Mock(ok=True, status_code=500)
+    x = elq.GetFields(entity='contacts')
