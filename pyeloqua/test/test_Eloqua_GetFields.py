24b7dd2d806f40bb4f140fb6ca1e68aa1d8c59f7
colemanja91 <colemanja91@gmail.com>
2016-11-21 20:46:36 -0500

    check EntityException

--- pyeloqua/test/test_Eloqua_GetFields.py
+++ pyeloqua/test/test_Eloqua_GetFields.py
@@ -5,5 +5,10 @@ import requests
 from pyeloqua import Eloqua
 from .test_successfulInit import elqLogin
 
+@patch('pyeloqua.pyeloqua.requests.get')
 @raises(Exception)
-def test_GetFields_
+def test_GetFields_EntityException(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    x = elq.GetFields(entity='bad')
