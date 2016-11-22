385e327b091a8d055a308c2ce933509c0631b53f
colemanja91 <colemanja91@gmail.com>
2016-11-21 19:38:34 -0500

    initial exception tests

--- pyeloqua/test/test_Eloqua_init.py
+++ pyeloqua/test/test_Eloqua_init.py
@@ -3,3 +3,14 @@ from mock import patch, Mock
 from pyeloqua import Eloqua
 
 # Test basic functions around Eloqua class
+@raises(Exception)
+def test_EloquaInit_MissingUsername():
+    elq = Eloqua(company = 'test', password = 'test')
+
+@raises(Exception)
+def test_EloquaInit_MissingCompany():
+    elq = Eloqua(username = 'test', password = 'test')
+
+@raises(Exception)
+def test_EloquaInit_MissingPassword():
+    elq = Eloqua(company = 'test', username = 'test')
