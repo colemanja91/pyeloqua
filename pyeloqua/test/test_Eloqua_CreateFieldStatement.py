3ce5478fdd63d8ae0e6769fc8a4dbea2d6bd4d35
colemanja91 <colemanja91@gmail.com>
2016-12-01 14:43:55 -0500

    ive made a huge mistake

--- pyeloqua/test/test_Eloqua_CreateFieldStatement.py
+++
@@ -1,7 +0,0 @@
-from nose.tools import *
-from mock import patch, Mock
-
-import requests
-from pyeloqua import Eloqua
-from .test_successfulInit import elqLogin
-from .test_Eloqua_GetFields_response import contactFieldsResponse, contactFieldsResult
