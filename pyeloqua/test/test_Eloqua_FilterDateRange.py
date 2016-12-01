e8c97375091c1fd3d9f0c534971e243c307bf3e9
colemanja91 <colemanja91@gmail.com>
2016-12-01 15:44:20 -0500

    added more tests but theyre wrong

--- pyeloqua/test/test_Eloqua_FilterDateRange.py
+++ pyeloqua/test/test_Eloqua_FilterDateRange.py
@@ -14,6 +14,38 @@ def test_FilterDateRange_StartEndBlank(mock_get):
     elq = Eloqua(company = 'test', username = 'test', password = 'test')
     x = elq.FilterDateRange(entity='contacts', field='CreatedAt')
 
+@patch('pyeloqua.pyeloqua.requests.get')
+@raises(Exception)
+def test_FilterDateRange_FieldBlank(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    x = elq.FilterDateRange(entity='contacts', field='', start='2016-10-10 01:00:00', end='2016-10-11 01:00:00')
+
+@patch('pyeloqua.pyeloqua.requests.get')
+@raises(Exception)
+def test_FilterDateRange_InvalidEntity(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    x = elq.FilterDateRange(entity='bad', field='CreatedAt', start='2016-10-10 01:00:00', end='2016-10-11 01:00:00')
+
+@patch('pyeloqua.pyeloqua.requests.get')
+@raises(Exception)
+def test_FilterDateRange_StartBadFormat(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    x = elq.FilterDateRange(entity='contacts', field='CreatedAt', start='something random', end='2016-10-11 01:00:00')
+
+@patch('pyeloqua.pyeloqua.requests.get')
+@raises(Exception)
+def test_FilterDateRange_EndBadFormat(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    x = elq.FilterDateRange(entity='contacts', field='CreatedAt', start='2016-10-10 01:00:00', end='something random')
+
 # @patch('pyeloqua.pyeloqua.requests.get')
 # def test_FilterExists_OneMatchFound(mock_get):
 #     mock_get.return_value = Mock(ok=True, status_code=200)
