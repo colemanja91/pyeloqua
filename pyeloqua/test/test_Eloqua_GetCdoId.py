d06fdb05b4c56aaca6a08399e33ecf4163daf2e2
colemanja91 <colemanja91@gmail.com>
2016-12-01 14:54:44 -0500

    initial responses and one test

--- pyeloqua/test/test_Eloqua_GetCdoId.py
+++ pyeloqua/test/test_Eloqua_GetCdoId.py
@@ -4,7 +4,7 @@ from mock import patch, Mock
 import requests
 from pyeloqua import Eloqua
 from .test_successfulInit import elqLogin
-from .test_Eloqua_GetFields_response import contactFieldsResponse, contactFieldsResult
+from .test_Eloqua_GetCdoId_response import cdoResultOne, cdoResultNone, cdoResultMany
 
 @patch('pyeloqua.pyeloqua.requests.get')
 @raises(Exception)
@@ -13,3 +13,11 @@ def test_GetCdoId_CdoNameRequired(mock_get):
     mock_get.return_value.json.return_value = elqLogin
     elq = Eloqua(company = 'test', username = 'test', password = 'test')
     x = elq.GetCdoId()
+
+@patch('pyeloqua.pyeloqua.requests.get')
+@raises(Exception)
+def test_GetCdoId_OneMatchFound(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    x = elq.GetCdoId(cdoName='test')
