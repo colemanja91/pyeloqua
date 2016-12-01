3038805ad1ae6d810424c37772c1ac612ce2e121
colemanja91 <colemanja91@gmail.com>
2016-12-01 15:27:00 -0500

    finished tests for FilterExists

--- pyeloqua/test/test_Eloqua_FilterExists.py
+++ pyeloqua/test/test_Eloqua_FilterExists.py
@@ -4,42 +4,42 @@ from mock import patch, Mock
 import requests
 from pyeloqua import Eloqua
 from .test_successfulInit import elqLogin
-from .test_Eloqua_GetCdoId_response import cdoResultOne, cdoResultNone, cdoResultMany
+from .test_Eloqua_FilterExists_response import filterResultOne, filterResultNone, filterResultMany
 
 @patch('pyeloqua.pyeloqua.requests.get')
 @raises(Exception)
-def test_GetCdoId_CdoNameRequired(mock_get):
+def test_FilterExists_BadExistsType(mock_get):
     mock_get.return_value = Mock(ok=True, status_code=200)
     mock_get.return_value.json.return_value = elqLogin
     elq = Eloqua(company = 'test', username = 'test', password = 'test')
-    x = elq.GetCdoId()
-#
-# @patch('pyeloqua.pyeloqua.requests.get')
-# def test_GetCdoId_OneMatchFound(mock_get):
-#     mock_get.return_value = Mock(ok=True, status_code=200)
-#     mock_get.return_value.json.return_value = elqLogin
-#     elq = Eloqua(company = 'test', username = 'test', password = 'test')
-#     mock_get.return_value = Mock(ok=True, status_code=200)
-#     mock_get.return_value.json.return_value = cdoResultOne
-#     x = elq.GetCdoId(cdoName='test')
-#     assert x==123
-#
-# @patch('pyeloqua.pyeloqua.requests.get')
-# @raises(Exception)
-# def test_GetCdoId_ManyMatchFound(mock_get):
-#     mock_get.return_value = Mock(ok=True, status_code=200)
-#     mock_get.return_value.json.return_value = elqLogin
-#     elq = Eloqua(company = 'test', username = 'test', password = 'test')
-#     mock_get.return_value = Mock(ok=True, status_code=200)
-#     mock_get.return_value.json.return_value = cdoResultMany
-#     x = elq.GetCdoId(cdoName='test')
-#
-# @patch('pyeloqua.pyeloqua.requests.get')
-# @raises(Exception)
-# def test_GetCdoId_NoneMatchFound(mock_get):
-#     mock_get.return_value = Mock(ok=True, status_code=200)
-#     mock_get.return_value.json.return_value = elqLogin
-#     elq = Eloqua(company = 'test', username = 'test', password = 'test')
-#     mock_get.return_value = Mock(ok=True, status_code=200)
-#     mock_get.return_value.json.return_value = cdoResultNone
-#     x = elq.GetCdoId(cdoName='test')
+    x = elq.FilterExists(name='test', existsType='bad')
+
+@patch('pyeloqua.pyeloqua.requests.get')
+def test_FilterExists_OneMatchFound(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = filterResultOne
+    x = elq.FilterExists(name='test', existsType='ContactList')
+    assert x=="EXISTS('{{ContactList[1]}}')"
+
+@patch('pyeloqua.pyeloqua.requests.get')
+@raises(Exception)
+def test_FilterExists_ManyMatchFound(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = filterResultMany
+    x = elq.FilterExists(name='test', existsType='ContactList')
+
+@patch('pyeloqua.pyeloqua.requests.get')
+@raises(Exception)
+def test_FilterExists_NoneMatchFound(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = filterResultNone
+    x = elq.FilterExists(name='test', existsType='ContactList')
--- pyeloqua/test/test_Eloqua_FilterExists_response.py
+++ pyeloqua/test/test_Eloqua_FilterExists_response.py
@@ -1,14 +1,14 @@
-cdoResultOne = {
+filterResultOne = {
   "items": [
     {
       "name": "test",
-      "displayNameFieldUri": "/customObjects/123/fields/111",
-      "emailAddressFieldUri": "/customObjects/123/fields/111",
-      "uri": "/customObjects/123",
+      "count": 0,
+      "statement": "{{ContactList[1]}}",
+      "uri": "/contacts/lists/1",
       "createdBy": "Test.User",
-      "createdAt": "2013-05-13T12:26:32.5930000Z",
+      "createdAt": "2016-09-28T19:02:39.5330000Z",
       "updatedBy": "Test.User",
-      "updatedAt": "2016-10-11T21:35:46.1670000Z"
+      "updatedAt": "2016-11-29T18:31:49.1800000Z"
     }
   ],
   "totalResults": 1,
@@ -18,7 +18,7 @@ cdoResultOne = {
   "hasMore": False
 }
 
-cdoResultNone = {
+filterResultNone = {
   "items": [],
   "totalResults": 0,
   "limit": 1000,
@@ -27,27 +27,27 @@ cdoResultNone = {
   "hasMore": False
 }
 
-cdoResultMany = {
+filterResultMany = {
   "items": [
     {
       "name": "test",
-      "displayNameFieldUri": "/customObjects/123/fields/111",
-      "emailAddressFieldUri": "/customObjects/123/fields/111",
-      "uri": "/customObjects/123",
+      "count": 0,
+      "statement": "{{ContactList[1]}}",
+      "uri": "/contacts/lists/1",
       "createdBy": "Test.User",
-      "createdAt": "2013-05-13T12:26:32.5930000Z",
+      "createdAt": "2016-09-28T19:02:39.5330000Z",
       "updatedBy": "Test.User",
-      "updatedAt": "2016-10-11T21:35:46.1670000Z"
+      "updatedAt": "2016-11-29T18:31:49.1800000Z"
     },
     {
       "name": "test2",
-      "displayNameFieldUri": "/customObjects/456/fields/222",
-      "emailAddressFieldUri": "/customObjects/456/fields/222",
-      "uri": "/customObjects/456",
+      "count": 0,
+      "statement": "{{ContactList[2]}}",
+      "uri": "/contacts/lists/2",
       "createdBy": "Test.User",
-      "createdAt": "2013-05-13T12:26:32.5930000Z",
+      "createdAt": "2016-09-28T19:02:39.5330000Z",
       "updatedBy": "Test.User",
-      "updatedAt": "2016-10-11T21:35:46.1670000Z"
+      "updatedAt": "2016-11-29T18:31:49.1800000Z"
     }
   ],
   "totalResults": 2,
