4c4b42bb8fdb2f9db81f91a80d8566d6d52d1443
colemanja91 <colemanja91@gmail.com>
2016-12-01 17:56:25 -0500

    test for FilterDateRange with no start/end

--- pyeloqua/test/test_Eloqua_FilterDateRange.py
+++ pyeloqua/test/test_Eloqua_FilterDateRange.py
@@ -53,3 +53,35 @@ def test_FilterDateRange_Activity(mock_get):
     elq = Eloqua(company = 'test', username = 'test', password = 'test')
     x = elq.FilterDateRange(entity='activities', start='2016-10-10 01:00:00', end='2016-10-11 01:00:00')
     assert x==" '{{Activity.CreatedAt}}' >= '2016-10-10 01:00:00'  AND  '{{Activity.CreatedAt}}' <= '2016-10-11 01:00:00' "
+
+@patch('pyeloqua.pyeloqua.requests.get')
+def test_FilterDateRange_ActivityNoStart(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    x = elq.FilterDateRange(entity='activities', end='2016-10-11 01:00:00')
+    assert x==" '{{Activity.CreatedAt}}' <= '2016-10-11 01:00:00' "
+
+@patch('pyeloqua.pyeloqua.requests.get')
+def test_FilterDateRange_ActivityNoEnd(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    x = elq.FilterDateRange(entity='activities', start='2016-10-10 01:00:00')
+    assert x==" '{{Activity.CreatedAt}}' >= '2016-10-10 01:00:00' "
+
+@patch('pyeloqua.pyeloqua.requests.get')
+def test_FilterDateRange_ContactsCreatedAt(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    x = elq.FilterDateRange(entity='contacts', field='createdAt', start='2016-10-10 01:00:00', end='2016-10-11 01:00:00')
+    assert x==" '{{Contact.CreatedAt}}' >= '2016-10-10 01:00:00'  AND  '{{Contact.CreatedAt}}' <= '2016-10-11 01:00:00' "
+
+@patch('pyeloqua.pyeloqua.requests.get')
+def test_FilterDateRange_ContactsUpdatedAt(mock_get):
+    mock_get.return_value = Mock(ok=True, status_code=200)
+    mock_get.return_value.json.return_value = elqLogin
+    elq = Eloqua(company = 'test', username = 'test', password = 'test')
+    x = elq.FilterDateRange(entity='contacts', field='updatedAt', start='2016-10-10 01:00:00', end='2016-10-11 01:00:00')
+    assert x==" '{{Contact.UpdatedAt}}' >= '2016-10-10 01:00:00'  AND  '{{Contact.UpdatedAt}}' <= '2016-10-11 01:00:00' "
