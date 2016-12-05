63a0dd028f0a1ecf09f0eca33167a759a1f45c7c
colemanja91 <colemanja91@gmail.com>
2016-12-05 09:35:21 -0500

    test CreateDef Exports CustomObjects

--- pyeloqua/test/test_Eloqua_CreateDef_response.py
+++ pyeloqua/test/test_Eloqua_CreateDef_response.py
@@ -37,3 +37,16 @@ export_accounts = {
     "updatedBy":"Test.User",
     "updatedAt":"2015-01-01T11:09:00.0000004Z"
 }
+
+export_customobjects = {
+    "name":"test",
+    "fields":{
+        "ID":"{{CustomObject[1].ExternalId}}"
+    },
+    "dataRetentionDuration":"P7D",
+    "uri":"/customObjects/1/exports/1234",
+    "createdBy":"Test.User",
+    "createdAt":"2015-01-01T11:09:00.0000004Z",
+    "updatedBy":"Test.User",
+    "updatedAt":"2015-01-01T11:09:00.0000004Z"
+}
