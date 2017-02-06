e1152cdcfacaf8e062ed6fc6c17a5cc7fc4b4762
colemanja91 <colemanja91@gmail.com>
2017-02-06 06:56:33 -0500

    start Import clas

--- pyeloqua/test/test_bulk_init.py
+++ pyeloqua/test/test_bulk_init.py
@@ -9,11 +9,16 @@ def test_bulk_init():
     """ bulk class initializes """
     assert Bulk(test=True) is not None
 
-def test_bulk_elqinit():
+def test_bulk_iselq():
     """ Is an Eloqua instance """
     bulk = Bulk(test=True)
     assert isinstance(bulk, Eloqua)
 
 ###############################################################################
-# config options
+# Nested class init
 ###############################################################################
+
+def test_import_init():
+    """ import class initializes """
+    bulk = Bulk(test=True)
+    assert bulk.Import() is not None
