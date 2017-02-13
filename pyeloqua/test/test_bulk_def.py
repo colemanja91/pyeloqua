9fb1733ce44432d2cf3ed9aaf2a42f875dc6fe35
colemanja91 <colemanja91@gmail.com>
2017-02-13 12:37:02 -0500

    for some reason one test fails, but only occasionally

--- pyeloqua/test/test_bulk_def.py
+++ pyeloqua/test/test_bulk_def.py
@@ -47,6 +47,16 @@ JOB_IMPORTS_CONTACTS = {
     }
 }
 
+JOB_IMPORTS_CONTACTS_BAD = {
+    'filters': [],
+    'fields': CONTACT_SYSTEM_FIELDS,
+    'job_type': 'imports',
+    'elq_object': 'contacts',
+    'obj_id': None,
+    'act_type': None,
+    'options': {}
+}
+
 DATA_IMPORTS_CONTACTS = {
     'name': 'test name',
     'fields': {
@@ -79,24 +89,34 @@ EXPORT_JOB_RESPONSE = {
 }
 
 IMPORT_JOB_RESPONSE = {
-  "name": "test name",
-  "fields": {
-    "contactID": "{{Contact.Id}}",
-    "createdAt": "{{Contact.CreatedAt}}",
-    "updatedAt": "{{Contact.UpdatedAt}}",
-    "isSubscribed": "{{Contact.Email.IsSubscribed}}",
-    "isBounced": "{{Contact.Email.IsBounced}}",
-    "emailFormat": "{{Contact.Email.Format}}"
-  },
-  "identifierFieldName": "contactID",
-  "isSyncTriggeredOnImport": False,
-  "dataRetentionDuration": "P7D",
-  "isUpdatingMultipleMatchedRecords": False,
-  "uri": "/contacts/imports/1",
-  "createdBy": "testuser",
-  "createdAt": "2017-02-13T16:38:13.3442894Z",
-  "updatedBy": "testuser",
-  "updatedAt": "2017-02-13T16:38:13.3442894Z"
+    "name": "test name",
+    "fields": {
+        "contactID": "{{Contact.Id}}",
+        "createdAt": "{{Contact.CreatedAt}}",
+        "updatedAt": "{{Contact.UpdatedAt}}",
+        "isSubscribed": "{{Contact.Email.IsSubscribed}}",
+        "isBounced": "{{Contact.Email.IsBounced}}",
+        "emailFormat": "{{Contact.Email.Format}}"
+    },
+    "identifierFieldName": "contactID",
+    "isSyncTriggeredOnImport": False,
+    "dataRetentionDuration": "P7D",
+    "isUpdatingMultipleMatchedRecords": False,
+    "uri": "/contacts/imports/1",
+    "createdBy": "testuser",
+    "createdAt": "2017-02-13T16:38:13.3442894Z",
+    "updatedBy": "testuser",
+    "updatedAt": "2017-02-13T16:38:13.3442894Z"
+}
+
+IMPORT_JOB_RESPONSE_BAD = {
+    "failures": [
+        {
+            "field": "identifierFieldName",
+            "constraint":
+            "Must be a string value, at least 1 character and at most 100 characters long."
+        }
+    ]
 }
 
 ###############################################################################
@@ -150,3 +170,15 @@ def test_create_imports_setdef(mock_post):
     mock_post.return_value.json.return_value = deepcopy(IMPORT_JOB_RESPONSE)
     bulk.create_def('test name')
     assert bulk.job_def == IMPORT_JOB_RESPONSE
+
+
+@patch('pyeloqua.bulk.requests.post')
+@raises(Exception)
+def test_create_imports_error(mock_post):
+    """ raise exception on bad import def """
+    bulk = Bulk(test=True)
+    bulk.job = JOB_IMPORTS_CONTACTS_BAD
+    mock_post.return_value = Mock(ok=True, status_code=400)
+    mock_post.return_value.json.return_value = deepcopy(
+        IMPORT_JOB_RESPONSE_BAD)
+    bulk.create_def('test name')
